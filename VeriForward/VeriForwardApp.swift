9b83aa84193da44905f6808075917aa5efe684de
Zach Sliefert <zsliefert@gmail.com>
2023-09-01 22:37:15 -0500

    Initial Commit

---
+++ VeriForward/VeriForwardApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeriForwardApp.swift
+//  VeriForward
+//
+//  Created by Zachary Sliefert on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct VeriForwardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
